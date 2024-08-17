1f7d195f7dcdf84dd43dc27b99308656c96629ce
Vanni <vaanichaudhary666@gmail.com>
2024-08-17 23:40:43 +0530

    project section
    added sample project cards in project section

---
+++ 01Portfolio/src/components/WorkCardData.jsx
@@ -0,0 +1,49 @@
+//local API
+
+import Pro1 from "../assets/pro1.jpeg"
+import Pro2 from "../assets/pro2.jpeg"
+import Pro3 from "../assets/pro3.jpeg"
+import Pro4 from "../assets/pro4.jpeg"
+import Pro5 from "../assets/pro5.jpeg"
+import Pro6 from "../assets/pro6.jpeg"
+
+const ProjectCardData = [
+    {
+        imgsrc: Pro1,
+        title: "Project Sample 1",
+        text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem excepturi sint asperiores magnam. Eos neque at quia vitae voluptatum ratione ullam, nulla, reprehenderit dolores perferendis illo. Rerum perspiciatis soluta voluptas?",
+        view: "www.google.in"
+    },
+    {
+        imgsrc: Pro2,
+        title: "Project Sample 2",
+        text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem excepturi sint asperiores magnam. Eos neque at quia vitae voluptatum ratione ullam, nulla, reprehenderit dolores perferendis illo. Rerum perspiciatis soluta voluptas?",
+        view: "www.google.in"
+    },
+    {
+        imgsrc: Pro3,
+        title: "Project Sample 3",
+        text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem excepturi sint asperiores magnam. Eos neque at quia vitae voluptatum ratione ullam, nulla, reprehenderit dolores perferendis illo. Rerum perspiciatis soluta voluptas?",
+        view: "www.google.in"
+    },
+    {
+        imgsrc: Pro4,
+        title: "Project Sample 4",
+        text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem excepturi sint asperiores magnam. Eos neque at quia vitae voluptatum ratione ullam, nulla, reprehenderit dolores perferendis illo. Rerum perspiciatis soluta voluptas?",
+        view: "www.google.in"
+    },
+    {
+        imgsrc: Pro5,
+        title: "Project Sample 5",
+        text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem excepturi sint asperiores magnam. Eos neque at quia vitae voluptatum ratione ullam, nulla, reprehenderit dolores perferendis illo. Rerum perspiciatis soluta voluptas?",
+        view: "www.google.in"
+    },
+    {
+        imgsrc: Pro6,
+        title: "Project Sample 6",
+        text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem excepturi sint asperiores magnam. Eos neque at quia vitae voluptatum ratione ullam, nulla, reprehenderit dolores perferendis illo. Rerum perspiciatis soluta voluptas?",
+        view: "www.google.in"
+    },
+];
+
+export default ProjectCardData;
--- 01Portfolio/src/routes/Project.jsx
+++ 01Portfolio/src/routes/Project.jsx
@@ -3,12 +3,14 @@ import React from 'react'
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer"; 
 import Hero2 from "../components/Hero2"; 
+import Work from "../components/Work"; 
 
 const Project = () => {
   return (
     <div>
       <Navbar />
       <Hero2 heading="PROJECTS." text="Some of my works." />
+      <Work />
       <Footer />
     </div>
   )
